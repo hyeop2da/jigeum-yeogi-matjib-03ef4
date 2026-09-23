aa4dc40c2626da32fd10a42d53a3a3fc76445904
hyeop2da <splendidlife87@gmail.com>
2026-09-23 10:23:31 +0900

    Prepare GitHub Pages frontend with existing rating proxy

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
   KAKAO_JS_KEY: 'cf02875c74d4f95e1f49e9ebf71bb9cb',
-  KAKAO_PROXY_URL: ''
+  KAKAO_PROXY_URL: 'https://ubiquitous-meerkat-e953f4.netlify.app/api/kakao-ratings'
 };
